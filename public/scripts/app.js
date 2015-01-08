13fe5595bbcb4ad82664e4a40e87b17eaa88ecb7
Ed Atrero <eatrero@gmail.com>
2015-01-07 22:40:46 -0800

    fw13 pipeline

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,50 @@
-var app = angular.module("app",[ ]);
+var app = angular.module("app",["firebase"]);
 
-app.controller("todoController", function($scope, $http){ 
+app.controller("todoController", ["$scope", "$http", "$firebase",
+	function($scope, $http, $firebase){
+		var obj = $firebase(new Firebase("https://glaring-heat-2011.firebaseio.com")).$asObject();
 
-	$scope.greeting = "Hello World. This is my todo app";
+		obj.$loaded().then(function() {
+			console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
 
-	updateTodoList();
+			// To iterate the key/value pairs of the object, use `angular.forEach()`
+			angular.forEach(obj, function(value, key) {
+				console.log(key, value);
+			});
 
-	function updateTodoList(){
-		$http.get('http://localhost:3245/item')
-		.success(function(result){
-			$scope.todos = result;
-			console.log(result);
+			$scope.students = obj.students;
+
+			console.log($scope.students);
 		});
+//		updateTodoList();
+
+		function updateTodoList(){
+			$http.get('http://localhost:3245/item')
+			.success(function(result){
+				$scope.todos = result;
+				console.log(result);
+			});
+		}
+
+		$scope.postTodo = function(todo){
+			console.log(todo.item);
+
+			$http.post('http://localhost:3245/item',
+			{item : todo.item})
+			.success(function(result){
+				// refresh list
+				updateTodoList();
+			});
+		};
+
+		$scope.deleteTodo = function(todo){
+
+			$http.delete('http://localhost:3245/item/'+todo._id)
+			.success(function(result){
+				// refresh list
+				updateTodoList();
+			});
+		};
+
 	}
-
-	$scope.postTodo = function(todo){
-		console.log(todo.item);
-
-		$http.post('http://localhost:3245/item', 
-		{item : todo.item})
-		.success(function(result){
-			// refresh list
-			updateTodoList();
-		});
-	};
-
-	$scope.deleteTodo = function(todo){
-
-		$http.delete('http://localhost:3245/item/'+todo._id) 
-		.success(function(result){
-			// refresh list
-			updateTodoList();
-		});
-	};
-
-});
+]);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,35 +1,36 @@
 'use strict'
 module.exports = function(app, mongoConnection){
 
-	var Ctrl = require('../controllers');
-	var ctrl = new Ctrl(mongoConnection);
+	var Student = require('../controllers/students');
+	var student = new Student(mongoConnection);
+	var Task = require('../controllers/tasks');
+	var task = new Task(mongoConnection);
 
 	app.get('/', function(req, res) {
 		res.render('index.ejs');
 	});
 
-	app.get('/item', function(req, res) {
-		ctrl.list(req, res);
+	app.get('/student', function(req, res) {
+		student.list(req, res);
 	});
 
-	app.post('/item', function(req, res) {
-		ctrl.create(req, res);
+	app.post('/student', function(req, res) {
+		student.create(req, res);
 	});
 
-	app.get('/item/:idx', function(req, res) {
+	app.get('/student/:idx', function(req, res) {
 		console.log('got get :idx');
-		ctrl.read(req, res);
+		student.read(req, res);
 	});
 
-	app.put('/item/:idx', function(req, res) {
+	app.put('/student/:idx', function(req, res) {
 		console.log('got put :idx');
-		ctrl.update(req, res);
+		student.update(req, res);
 	});
 
-	app.delete('/item/:idx', function(req, res) {
+	app.delete('/student/:idx', function(req, res) {
 		console.log('got delete :idx');
-		ctrl.delete(req, res);
+		student.delete(req, res);
 	});
-
 
 };
